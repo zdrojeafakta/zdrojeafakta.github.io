3abdeb9827e5cd2b84736a7ac48283d1e395db64
zdrojeafakta <zdrojeafakta@gmail.com>
2017-05-01 09:45:55 +0200

    upated loader for github

--- js/loader.js
+++ js/loader.js
@@ -5,6 +5,8 @@ define(["require", "exports", "parsers/parser", "html/html", "boot/navigation", 
         init() {
             let pathname = window.location.pathname;
             let name = pathname.replace(".html", ".txt");
+            if (name = "/")
+                name = "index.txt";
             console.log(name);
             // Process text file
             var stringData = $.ajax({
